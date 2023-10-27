ed5bb5b0af8845493e1550fb0163c847588e8b32
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-27 13:25:01 +0300

    init

--- task9/ContentView.swift
+++ task9/ContentView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offSet: CGSize = .zero
+    
+    private var point: CGPoint {
+        CGPoint(x: UIScreen.main.bounds.width / 2.0,y: UIScreen.main.bounds.height / 2.0)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Canvas { context, size in
+            let shapeOne = context.resolveSymbol(id: 1)!
+            let shapeTwo = context.resolveSymbol(id: 2)!
+            
+            context.addFilter(.alphaThreshold(min: 0.5, color:.blue))
+            
+            context.addFilter(.blur(radius: 25))
+            
+            context.drawLayer { context in
+                context.draw(shapeOne, at: point)
+                context.draw(shapeTwo, at: point)
+            }
+        } symbols: {
+            Ellipse()
+                .frame(width: 200, height: 200)
+                .tag(1)
+            Ellipse()
+                .frame(width: 200, height: 200)
+                .offset(x: offSet.width, y: offSet.height)
+                .tag(2)
         }
-        .padding()
+        .ignoresSafeArea()
+        .gesture(DragGesture()
+            .onChanged { value in
+                offSet = value.translation
+            }.onEnded { value in
+                withAnimation(.spring(bounce: 0.3)) {
+                    offSet = .zero
+                }
+            })
     }
 }
+
 
 #Preview {
     ContentView()
